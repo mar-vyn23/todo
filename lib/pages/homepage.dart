82998af3aa001fb4589d6a9d76ecc194463a65bf
mar-vyn23 <msatulo23@gmail.com>
2025-02-14 22:47:44 +0300

    feat: convert HomePage to StatefulWidget and implement task list functionality

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:todo/utils/todo_tile.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget{
   HomePage({super.key}){
     debugPrint("Todo App created");
   }
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // list to do tasks
+  List toDolist = [
+    ["Code", false],
+    ["Study", false],
+  ];
+
+  void checkBoxchanged(bool? value, int index){
+    setState(() {
+      toDolist[index][1] = !toDolist[index][1];
+    });
+  }
+
 
   @override
   Widget build(BuildContext context){
@@ -18,16 +36,16 @@ class HomePage extends StatelessWidget{
           elevation: 0,
         ),
         backgroundColor: const Color.fromARGB(255, 236, 226, 136),
-        body: ListView(
-          children: [
-            TodoTile(
-              taskName: "Finish ToDo app",
-              taskCompleted: true,
-              onChanged: (p0){},
-            ),
-          ],
+        body: ListView.builder(
+          itemCount: toDolist.length,
+          itemBuilder: (context, index) => TodoTile(
+            taskName: toDolist[index][0], 
+            taskCompleted: toDolist[index][1], 
+            onChanged: (value) => checkBoxchanged(value, index),
         ),
       ),
-    );
+    )
+    )
+    ;
   }
 }
--- lib/utils/todo_tile.dart
+++ lib/utils/todo_tile.dart
@@ -31,9 +31,7 @@ class TodoTile extends StatelessWidget {
             //task name
             Text(taskName),
           ],
-        ),
-        
-          
+        ),  
       ),
     );
   }
